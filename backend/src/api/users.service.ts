576b2630519806c3132b43bea0ef8d39f4d02525
KChan-ka <thuntor@hotmail.com>
2025-04-16 00:29:19 +0100

    minor fix

--- backend/src/api/users.service.ts
+++ backend/src/api/users.service.ts
@@ -7,49 +7,22 @@ import { User, Prisma } from '@prisma/client';
 @Injectable()
 export class UsersService {
 
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
-  async getAllUsers(): Promise<User[]> {
-    return this.prisma.user.findMany();
-  }
-
-  async getUserById(userId): Promise<User | null> {
-    const user = await this.prisma.user.findUnique({
-      where: { user_id: userId },
-    });
-    if (!user) {
-      throw new NotFoundException(`User_id ${userId} was not found`);
-
+    async getAllUsers(): Promise<User[]> {
+        return this.prisma.user.findMany();
     }
-    return user;
-  }
 
+    async getUserById(userId): Promise<User | null> {
+        const user = await this.prisma.user.findUnique({
+            where: { user_id: userId },
+        });
+        if (!user) {
+            throw new NotFoundException(`User_id ${userId} was not found`);
 
-  async getUserEvents(userId: number, statusQuery?: number[]) {
-    if (statusQuery) {
-      return this.prisma.event.findMany({
-        where: {
-          UserEvent: {
-            some: {
-              user_id: userId,
-              status_id: { in: statusQuery },
-            },
-          },
-        },
-      });
-    } else {
-      return this.prisma.event.findMany({
-        where: {
-          UserEvent: {
-            some: {
-              user_id: userId,
-            },
-          },
-        },
-      });
+        }
+        return user;
     }
-  }
-
 
     async getUserEvents(userId: number, statusQuery?: number[]) {
         if (statusQuery) {
