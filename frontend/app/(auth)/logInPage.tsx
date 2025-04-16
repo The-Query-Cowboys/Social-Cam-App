92a43d2a7dd75f8fdc659baa48dd8ac6fa0edaec
kakvo <kakvoo1@gmail.com>
2025-04-16 16:54:21 +0100

    Implemented clerk authentication

--- frontend/app/(auth)/logInPage.tsx
+++ frontend/app/(auth)/logInPage.tsx
@@ -1,26 +1,38 @@
-import {Text, View, TextInput, Button} from 'react-native';
-import {Link} from 'expo-router';
-import {useState} from 'react';
+import { Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { Link, useRouter } from 'expo-router';
+import { useState } from 'react';
 import React from 'react';
-import {useTheme} from "@/context/ThemeContext";
+import { useTheme } from "@/context/ThemeContext";
+import { useSignIn } from "@clerk/clerk-expo";
 
 const logInPage = () => {
     const {colorScheme} = useTheme()
-
-    const applyViewTheme = `${colorScheme === 'dark' ? 'text-white bg-black' : 'text-black bg-white'}`
-    const applyTheme = `${colorScheme === 'dark' ? 'text-white bg-black w-full p-3 border border-white rounded-lg' : 'text-black bg-white w-full p-3 border border-black rounded-lg'}`
+    const { signIn, setActive, isLoaded } = useSignIn();
+    const router = useRouter();
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    const onClick = () => {
-        console.log(username, password)
-        if (username === 'dev' && password === 'dev') {
-            console.log('Login successful')
-        } else {
-            console.log('Login failed')
+    const applyViewTheme = `${colorScheme === 'dark' ? 'text-white bg-black' : 'text-black bg-white'}`
+    const applyTheme = `${colorScheme === 'dark' ? 'text-white bg-black w-full p-3 border border-white rounded-lg' : 'text-black bg-white w-full p-3 border border-black rounded-lg'}`
+
+    const onSignInPress = async () => {
+        if (!isLoaded) return;
+        try {
+            const signInAttempt = await signIn.create({
+                identifier: username, password
+            });
+            if (signInAttempt.status === "complete") {
+                await setActive({ session: signInAttempt.createdSessionId });
+                console.log(signInAttempt, 'here'); //
+                router.replace("/");
+            } else {
+                console.error(JSON.stringify(signInAttempt, null, 2), 'here2'); //
+            }
+        } catch (err) {
+            console.error(JSON.stringify(err, null, 2));
         }
-    }
+    };
 
     return (
         <View className={`flex-1 items-center justify-center ${applyViewTheme}`}>
@@ -30,14 +42,16 @@ const logInPage = () => {
             <View>
                 <TextInput className={`text-l mb-5 ${applyTheme}`}
                            placeholder='Username'
-                           onChangeText={setUsername}
+                           onChangeText={(username) => setUsername(username)}
+                           value={username}
                 />
                 <TextInput className={`text-l mb-5 ${applyTheme}`}
                            secureTextEntry={true}
                            placeholder='Password'
-                           onChangeText={setPassword}
+                           onChangeText={(password) => setPassword(password)}
+                           value={password}
                 />
-                <Button title='Login' onPress={onClick}/>
+                <Button title='Login' onPress={onSignInPress}/>
 
                 <Text className={`${applyViewTheme}`}>Don't have an account?
                     <Link href='/signUpPage' className={`color-blue-500 font-bold underline m-1 ${applyViewTheme}`}>
